00803018dacc8edcaba43c372ffc170568404235
magus <noah@iamnoah.com>
2020-09-26 19:57:34 -0700

    FSUtils: append

--- utils/fs.js
+++ utils/fs.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
 
 exports.read = (filename) => fs.readFileSync(filename).toString();
+exports.append = (filename, content) => fs.appendFileSync(filename, content);
 exports.write = (filename, content) => fs.writeFileSync(filename, content);
