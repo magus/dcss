fff8e565f05b6777bb160f1043d682b4cad2e8c8
magus <noah@iamnoah.com>
2020-11-25 23:28:56 -0800

    parseUser: parseMorgues @time

--- scripts/parseUser.js
+++ scripts/parseUser.js
@@ -48,60 +48,109 @@ async function run() {
   // console.debug(allMorgueFileContent.length, 'allMorgueFileContent');
   // console.debug(allMorgueFileContent[0]);
 
-  const searchResults = [];
+  if (search === '@time') {
+    const parseMorgueResults = await parseMorgues(
+      { spinner, allMorgueFileContent, hrStartTime },
+      async (morgue) => {
+        const timeSeconds = parseMorgueFile('@time', morgue.content);
+        return timeSeconds;
+      },
+      ({ results, searchTime }) => {
+        const totalTime = results.reduce((ts, r) => r.timeSeconds + ts, 0);
+        const readableTime = secondsToTimeUnits(totalTime);
+        console.info(chalk.bold(chalk.yellow(readableTime)));
+      },
+    );
 
-  const promseSearchAllContent = allMorgueFileContent.map(async (morgue) => {
-    return new Promise((resolve) => {
-      spinner.text = `Parsing ${morgue.filename}`;
-      spinner.render();
+    console.info(`🤖 ${'Done'}`);
+  } else {
+    await parseMorgues(
+      { spinner, allMorgueFileContent, hrStartTime },
+      async (morgue, cacheMorguePath) => {
+        return new Promise((resolve) => {
+          const grepOutput = exec(
+            `cat "${cacheMorguePath}" | grep -B 2 -A 2 -ie "${search}"`,
+            (err, stdout, stderr) => {
+              if (err) {
+                if (err.code === 1) {
+                  // Ignore non-zero exit code
+                  // grep returns a 1 exit code when there are no matches
+                } else {
+                  console.error(err);
+                }
+                return resolve(null);
+              }
 
-      const cacheMorguePath = `${CACHE_DIR}/${morgue.filename}`;
-      const grepOutput = exec(`cat "${cacheMorguePath}" | grep -B 2 -A 2 -ie "${search}"`, (err, stdout, stderr) => {
-        if (err) {
-          if (err.code === 1) {
-            // Ignore non-zero exit code
-            // grep returns a 1 exit code when there are no matches
-          } else {
-            console.error(err);
+              const grep = stdout.toString();
+              resolve({ morgue, grep });
+            },
+          );
+        });
+      },
+      (parseMorgueResults) => {
+        const searchResults = parseMorgueResults.results.filter((result) => {
+          // skip null (empty search results)
+          if (!result) {
+            return false;
           }
-          return resolve();
-        }
 
-        const grep = stdout.toString();
-        searchResults.push({ morgue, grep });
-        resolve();
-      });
-    });
+          return true;
+        });
+
+        const searchResultMessage = `${chalk.green(searchResults.length)} result${
+          searchResults.length > 1 ? 's' : ''
+        } found in ${chalk.magenta(allMorgueFileContent.length)} morgue files (${chalk.dim(
+          `${parseMorgueResults.searchTime}ms`,
+        )}).`;
+
+        spinner.succeed(searchResultMessage);
+        console.info();
+
+        searchResults.forEach((result, i) => {
+          const {
+            grep,
+            morgue: { filename },
+          } = result;
+
+          const cacheMorguePath = `${CACHE_DIR}/${filename}`;
+
+          console.info(`👉 ${chalk.magenta(i + 1)} [${chalk.cyan(filename)}]`);
+          console.info(chalk.dim(`${RAWDATA_PATH}/${username}/${filename}`));
+          console.info(chalk.dim(cacheMorguePath));
+
+          const wrappedSearchInGrep = grep.replace(new RegExp(search, 'ig'), (match) => {
+            return chalk.bold(chalk.yellow(match));
+          });
+          console.info();
+          console.debug(chalk.gray(wrappedSearchInGrep));
+        });
+
+        console.info(`🤖 ${searchResultMessage}`);
+      },
+    );
+  }
+}
+
+async function parseMorgues({ spinner, allMorgueFileContent, hrStartTime }, morgueCallback, resultCallback) {
+  const promiseSearchAllContent = allMorgueFileContent.map(async (morgue) => {
+    spinner.text = `Parsing ${morgue.filename}`;
+    spinner.render();
+
+    const cacheMorguePath = `${CACHE_DIR}/${morgue.filename}`;
+    return await morgueCallback(morgue, cacheMorguePath);
   });
 
-  await Promise.all(promseSearchAllContent);
-
+  const results = await Promise.all(promiseSearchAllContent);
   const searchTime = Math.round(hrTimeUnit(process.hrtime(hrStartTime), 'ms'));
-  const searchResultMessage = `${chalk.green(searchResults.length)} result${
-    searchResults.length > 1 ? 's' : ''
-  } (${chalk.dim(`${searchTime}ms`)}) found in ${chalk.magenta(allMorgueFileContent.length)} morgue files .`;
+
+  const searchResultMessage = `Searched ${chalk.magenta(allMorgueFileContent.length)} morgue files (${chalk.dim(
+    `${searchTime}ms`,
+  )}).`;
 
   spinner.succeed(searchResultMessage);
   console.info();
 
-  searchResults.forEach((result, i) => {
-    const {
-      grep,
-      morgue: { filename },
-    } = result;
-
-    const cacheMorguePath = `${CACHE_DIR}/${filename}`;
-
-    console.info(`👉 ${chalk.magenta(i + 1)} [${chalk.cyan(filename)}]`);
-    console.info(chalk.dim(`${RAWDATA_PATH}/${username}/${filename}`));
-    console.info(chalk.dim(cacheMorguePath));
-
-    const wrapedSearchInGrep = grep.replace(new RegExp(search, 'ig'), (match) => {
-      return chalk.bold(chalk.yellow(match));
-    });
-    console.info();
-    console.debug(chalk.gray(wrapedSearchInGrep));
-  });
+  resultCallback({ results, searchTime });
 
   console.info(`🤖 ${searchResultMessage}`);
 }
@@ -130,6 +179,7 @@ async function cachedMorgue(username, morgueFilename, spinner) {
     const respText = await resp.text();
 
     fs.writeFileSync(cacheMorguePath, respText);
+    return respText;
   }
 
   return fs.readFileSync(cacheMorguePath, { encoding: 'utf8', flag: 'r' });
@@ -174,5 +224,52 @@ const hrTimeUnit = (hrTime, unit) => {
       return hrTime[0] * 1e9 + hrTime[1];
   }
 };
+
+const toNumber = (value) => parseInt(value, 10);
+const zeroPad = (value) => (value < 10 ? `0${value}` : value);
+
+function secondsToTimeUnits(seconds) {
+  const hourSeconds = 60 * 60;
+  const minuteSeconds = 60;
+
+  const hours = Math.floor(seconds / hourSeconds);
+  // seconds % daySeconds % hourSeconds is seconds leftover for minutes
+  // therefor seconds leftover for minutes / minuteSeconds = minutes
+  const minutes = Math.floor((seconds % hourSeconds) / minuteSeconds);
+  const remainderSeconds = (seconds % hourSeconds) % minuteSeconds;
+
+  const totalHours = Math.floor(seconds / hourSeconds);
+
+  const units = [];
+
+  if (hours) {
+    units.push(`${hours} hours`);
+  }
+  if (minutes) {
+    units.push(`${minutes} minutes`);
+  }
+  if (remainderSeconds) {
+    units.push(`${remainderSeconds} seconds`);
+  }
+  return units.join(' ');
+}
+
+async function parseMorgueFile(type, morgueText) {
+  switch (type) {
+    case '@time': {
+      const match = morgueText.match(/Time: ([\d:]+)/);
+      if (match) {
+        const [, timeString] = match;
+        const [hours, minutes, seconds] = timeString.split(':').map(toNumber);
+        const timeSeconds = hours * 60 * 60 + minutes * 60 + seconds;
+        return { timeSeconds };
+      }
+
+      return { timeSeconds: 0 };
+    }
+    default:
+      return null;
+  }
+}
 
 run();
