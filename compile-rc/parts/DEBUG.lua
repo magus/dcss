5ae384cbabe869dc4710682aef3b80365eafbc4b
magus <noah@iamnoah.com>
2021-03-28 21:01:39 -0700

    DEBUG: barachi have water walking

--- compile-rc/parts/DEBUG.lua
+++ compile-rc/parts/DEBUG.lua
@@ -1,9 +1,9 @@
 -- rc_msg("DEBUG")
 
 -- rc_msg(you.race())
--- rc_msg(tostring(not table_has({"Merfolk", "Octopode"}, you.race())))
+-- rc_msg(tostring(not table_has({"Merfolk", "Octopode", "Barachi"}, you.race())))
 
--- if not table_has({"Merfolk", "Octopode"}, you.race()) then
+-- if not table_has({"Merfolk", "Octopode", "Barachi"}, you.race()) then
 --   rc_msg("1 Is NOT deep water walking race")
 -- else
 --   rc_msg("1 Is deep water walking race")
