314843ab6eec86fd96bed04a5bbcdede19e6ae60
magus <noah@iamnoah.com>
2021-04-04 01:48:18 -0700

    DEBUG: crawl.yesno for yes/no question and saving variable

--- compile-rc/parts/DEBUG.lua
+++ compile-rc/parts/DEBUG.lua
@@ -1,5 +1,19 @@
 -- rc_msg("DEBUG")
 
+-- yesno (prompt, safe[, safeanswer[, clear_after=true[, interrupt_delays=true[, noprompt=false]]]])
+-- http://doc.dcss.io/modules/crawl.html#yesno
+-- Ask the player a yes/no question. The player is supposed to answer by pressing Y or N.
+-- Parameters:
+-- prompt string question for the user
+-- safe boolean accept lowercase answers
+-- safeanswer string or nil if a letter, this will be considered a safe default (optional)
+-- clear_after boolean clear the question after the user answers (default true)
+-- interrupt_delays boolean interrupt any ongoing delays to ask the question (default true)
+-- noprompt boolean skip asking the question; just wait for the answer (default false)
+
+-- local result = crawl.yesno("Answer this question (result)")
+-- rc_msg(string.format("result=[%s]", tostring(result)))
+
 -- rc_msg(you.race())
 -- rc_msg(tostring(not table_has({"Merfolk", "Octopode", "Barachi"}, you.race())))
 
