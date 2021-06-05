955a8400251fb6669d4d3713270a28b184d311d1
magus <noah@iamnoah.com>
2021-06-05 13:31:36 -0700

    OpenSkills: do not open menu for gnoll

--- compile-rc/parts/OpenSkills.lua
+++ compile-rc/parts/OpenSkills.lua
@@ -1,5 +1,7 @@
 -- Open skills menu at start of runs
-local need_skills_opened = true
+local is_gnoll = table_has({"Gnoll"}, you.race())
+local need_skills_opened = not is_gnoll
+
 local function start_open_skills()
   if you.turns() == 0 and need_skills_opened then
     need_skills_opened = false
