78eb6f829c733ab14dfa18d82d75e1ec3a660cfb
magus <noah@iamnoah.com>
2021-12-16 21:43:53 -0800

    refactor(Globals): use nested Globals parts

---
+++ compile-rc/parts/Globals/Globals.lua
@@ -0,0 +1,20 @@
+-- Pause for AnnounceDamage.lua
+PAUSE_MORE = "PAUSE"
+
+-- Standard Colors (ignoring black, not visible)
+COLORS = {
+  ["darkgrey"] = "darkgrey",
+  ["lightgrey"] = "lightgrey",
+  ["white"] = "white",
+  ["blue"] = "blue",
+  ["lightblue"] = "lightblue",
+  ["green"] = "green",
+  ["lightgreen"] = "lightgreen",
+  ["cyan"] = "cyan",
+  ["lightcyan"] = "lightcyan",
+  ["red"] = "red",
+  ["lightred"] = "lightred",
+  ["magenta"] = "magenta",
+  ["lightmagenta"] = "lightmagenta",
+  ["yellow"] = "yellow",
+  ["brown"] = "brown"}
